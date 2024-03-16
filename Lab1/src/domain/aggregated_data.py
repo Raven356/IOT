cfa8e7d3a28e5b49b2e2200cf25428f1dde87396
retinolacetate <83028214+retinolacetate@users.noreply.github.com>
2024-03-16 21:35:13 +0200

    Fix field name

--- Lab1/src/domain/aggregated_data.py
+++ Lab1/src/domain/aggregated_data.py
@@ -4,9 +4,10 @@ from domain.accelerometer import Accelerometer
 from domain.gps import Gps
 from domain.parking import Parking
 
+
 @dataclass
 class AggregatedData:
     accelerometer: Accelerometer
     gps: Gps
     parking: Parking
-    time: datetime
+    timestamp: datetime
